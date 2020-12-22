4cdd6b1e279184adddb689bed12b8c2ef2097223
Marco <markoka1@outlook.it>
2020-12-22 17:31:29 +0100

    Definizione nomi delle rotte ed avanzamento

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,32 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        @yield('content')
+        <header>
+            <div class="logo">
+                <img src="{{ asset('images/la-molisana-logo.png') }}" alt="logo-molisana">
+            </div>
+            <div class="main-menu">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="{{route('home')}}">Home</a>
+                        </li>
+                        <li>
+                            <a href="{{route('prodotti')}}">Prodotti</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+
+        </footer>
         <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
     </body>
 </html>
